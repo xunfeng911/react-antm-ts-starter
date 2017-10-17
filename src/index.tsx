76639877575c24da60711ad0524e3c93b3a5c2e2
xunfeng911 <596927571@qq.com>
2017-10-17 22:30:38 +0800

    add mobx

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,20 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
+import { Provider } from 'mobx-react';
 import './index.css';
 import { AppContainer } from 'react-hot-loader';
 import registerServiceWorker from './registerServiceWorker';
 import RouteConfig from './route/index';
+import AppState from './mobx/appState';
 
+const appState = new AppState();
 const app = document.getElementById('root');
 const render = () => {
   ReactDOM.render(
     <AppContainer>
-       <RouteConfig />
+      <Provider appState={appState} >
+         <RouteConfig />
+      </Provider>
     </AppContainer>,
     app
   );
---
+++ src/mobx/types.ts
@@ -0,0 +1,10 @@
+export interface userData {
+  nickName: string;
+  name?: string;
+  sex?: number;
+  headImgUrl?: string;
+  id: number;
+  showNote: boolean;
+  isCloumn: boolean;
+  phoneNumber?: number;
+}
