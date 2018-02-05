7857cc5b831a07a2c67fff7149ac768e4521a9a4
xunfeng911 <596927571@qq.com>
2018-02-05 21:51:11 +0800

    ad

--- src/index.tsx
+++ src/index.tsx
@@ -1,27 +1,31 @@
-import * as React from 'react';
-import * as ReactDOM from 'react-dom';
-import { Provider } from 'mobx-react';
-import './index.css';
-import { AppContainer } from 'react-hot-loader';
-import registerServiceWorker from './registerServiceWorker';
-import RouteConfig from './route/index';
-import Store from './mobx/Store';
+// import * as React from 'react';
+// import * as ReactDOM from 'react-dom';
+// import { Provider } from 'mobx-react';
+// import './index.css';
+// import { AppContainer } from 'react-hot-loader';
+// import registerServiceWorker from './registerServiceWorker';
+// import RouteConfig from './route/index';
+// import Store from './mobx/Store';
 
-const store = new Store();
-const app = document.getElementById('root');
-const render = () => {
-  ReactDOM.render(
-    <AppContainer>
-      <Provider store={store} >
-         <RouteConfig />
-      </Provider>
-    </AppContainer>,
-    app
-  );
-};
-render();
+// const store = new Store();
+// console.log(store);
+// const app = document.getElementById('root');
+// const render = () => {
+//   ReactDOM.render(
+//     <AppContainer>
+//       <Provider store={store} >
+//          <RouteConfig />
+//       </Provider>
+//     </AppContainer>,
+//     app
+//   );
+// };
+// render();
 
-if (module.hot) {
-  module.hot.accept(`./route/index`, () => { render(); });
-}
-registerServiceWorker();
+// if (module.hot) {
+//   module.hot.accept(`./route/index`, () => { render(); });
+// }
+// registerServiceWorker();
+
+import * as Rx from 'rxjs';
+console.log(Rx);
